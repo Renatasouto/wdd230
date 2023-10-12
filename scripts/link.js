bc13c1366ad56bcf2559ec2e2a4578d96c923d1f
renatasouto <renatagabes@gmail.com>
2023-10-11 22:16:12 -0500

    final

--- scripts/link.js
+++ scripts/link.js
@@ -3,39 +3,49 @@ const baseURL = "renatasouto.github.io/wdd230/";
 const linksURL = "https://renatasouto.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
-  }
-  
-  function displayLinks(weeks) {
-    const linksContainer = document.getElementById('links-container');
-    
-    weeks.forEach((week) => {
-      const weekDiv = document.createElement('div');
-      weekDiv.className = 'week';
+  const response = await fetch(linksURL);
+  console.log('a'+response)
+  const data = await response.json();
+  console.log('b'+data)
+  console.log('c'+data.weeks)
+  displayLinks(data.weeks);
+}
+{/* <li>Week 01: 
+    <a class="colorLink" href="">Layout</a> | 
+    <a class="colorLink" href="https://renatasouto.github.io/wdd230/week01/media-query.html">Media Query</a> | 
+    <a class="colorLink" href="https://codepen.io/Renatasouto/pen/RwEVmer.html">JS Pen</a> | 
+    <a class="colorLink" href="https://renatasouto.github.io/wdd230/week01/chamber-site-plan.html">Chamber Site Plan</a>
+</li> */}
+
+function displayLinks(weeks) {
+  const ulContainer = document.getElementById('ul-container');
+  console.log(weeks)
+  weeks.forEach((week) => {
+   
+    const listItem = document.createElement('li');
+    listItem.textContent = week.week + ': ';
+
+    week.links.forEach((link, linkIndex) => {
       
-      const weekHeader = document.createElement('h2');
-      weekHeader.textContent = week.week;
+      const linkElement = document.createElement('a');
+      linkElement.className = 'colorLink';
+      linkElement.href = link.url;
+      linkElement.textContent = link.title;
       
-      const linksList = document.createElement('ul');
       
-      week.links.forEach((link) => {
-        const listItem = document.createElement('li');
-        const linkElement = document.createElement('a');
-        linkElement.href = link.url;
-        linkElement.textContent = link.title;
-        listItem.appendChild(linkElement);
-        linksList.appendChild(listItem);
-      });
-      
-      weekDiv.appendChild(weekHeader);
-      weekDiv.appendChild(linksList);
-      
-      linksContainer.appendChild(weekDiv);
+      listItem.appendChild(linkElement);
+      if (linkIndex < week.links.length - 1) {
+        const separator = document.createTextNode(' | ');
+        listItem.appendChild(separator);
+      }
+     
     });
-  }
-  
-  getLinks();
+    
+    ulContainer.appendChild(listItem);
+  });
+}
+
+getLinks();
+
   
 
