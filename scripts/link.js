a9df6e4e31ee1857d8cf505576a9093c128a469e
renatasouto <renatagabes@gmail.com>
2023-10-11 23:18:22 -0500

    final

--- scripts/link.js
+++ scripts/link.js
@@ -4,22 +4,15 @@ const linksURL = "https://renatasouto.github.io/wdd230/data/links.json";
 
 async function getLinks() {
   const response = await fetch(linksURL);
-  console.log('a'+response)
+ 
   const data = await response.json();
-  console.log('b'+data)
-  console.log('c'+data.weeks)
+  
   displayLinks(data.weeks);
 }
-{/* <li>Week 01: 
-    <a class="colorLink" href="">Layout</a> | 
-    <a class="colorLink" href="https://renatasouto.github.io/wdd230/week01/media-query.html">Media Query</a> | 
-    <a class="colorLink" href="https://codepen.io/Renatasouto/pen/RwEVmer.html">JS Pen</a> | 
-    <a class="colorLink" href="https://renatasouto.github.io/wdd230/week01/chamber-site-plan.html">Chamber Site Plan</a>
-</li> */}
 
 function displayLinks(weeks) {
   const ulContainer = document.getElementById('ul-container');
-  console.log(weeks)
+ 
   weeks.forEach((week) => {
    
     const listItem = document.createElement('li');
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,44 +1,41 @@
 const apiKey = '05f760683307858609be5a5de22e5c7b';
-
-
 const cityId = '3394023';
-
-
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=imperial`; // Alterado para unidades imperiais (Fahrenheit)
-
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=imperial`;
 
 const weatherCard = document.getElementById('weather-card');
 const weatherIcon = document.getElementById('weather-icon');
 const weatherDescription = document.getElementById('weather-description');
 const temperature = document.getElementById('temperature');
-const locationName = document.getElementById('location'); 
-
+const locationName = document.getElementById('location');
 
 async function getWeatherData() {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-        const fahrenheit = data.main.temp;
+    const fahrenheit = data.main.temp;
 
-        if (weatherIcon) {
-            weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        }
-
-        if (weatherDescription) {
-            weatherDescription.textContent = data.weather[0].description;
-        }
-
-        if (temperature) {
-            temperature.textContent = `${Math.round(fahrenheit)}°F`; 
-        }
-
-        if (locationName) {
-            locationName.textContent = data.name;
-        }
-    } catch (error) {
-        console.error('Error:', error);
+    if (weatherIcon) {
+      weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     }
+
+    if (weatherDescription) {
+      // Mapeie a descrição para uma descrição mais amigável
+
+      const description = data.weather[0].description;
+      weatherDescription.textContent = description;
+    }
+
+    if (temperature) {
+      temperature.textContent = `${Math.round(fahrenheit)}°F`;
+    }
+
+    if (locationName) {
+      locationName.textContent = data.name;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
 window.addEventListener('load', getWeatherData);
